5965133f73986902212ddcc7200a7989d4abab97
sergul <sergit001k@gmail.com>
2022-11-04 22:29:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const [lat, setLat] = useState(0);
+    const [long, setLong] = useState(0);
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            setLat(position.coords.latitude);
+            setLong(position.coords.longitude);
+        });
+
+        (async () => {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`);
+            setData(await response.json())
+        })()
+
+        console.log(data)
+    }, [lat, long]);
+
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
-
-export default App;
